dbe7eaa895a40263c083dd49b0f9e67587f3a455
kelvin <kelvinmusselli22@gmail.com>
2020-01-04 12:44:58 -0300

    criado get e post

--- NodeJS/modulo01/index.js
+++ NodeJS/modulo01/index.js
@@ -1,9 +1,32 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json()); //se não colocar este parametro vc nao consegue mandar body json
 
-server.get("/teste", (req, res) => {
-  return res.json({ name: [{ ola: "ola" }, { ola: "tudo bem" }] });
+server.get("/query", (req, res) => {
+  const nome = req.query.nome;
+  return res.json({ message: `Hello ${nome}` });
+});
+
+server.get("/routes/:id", (req, res) => {
+  const id = req.params.id;
+  return res.json({ message: `Hello User Você Me Encontrou => Id: ${id}` });
+});
+
+// VARIAVEL DOS USERS
+const users = ["Kelvin", "Marcos", "Joana"];
+// GET
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+// POST
+server.post("/user", (req, res) => {
+  // desestruturado;
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
 });
 
 server.listen(3000);
