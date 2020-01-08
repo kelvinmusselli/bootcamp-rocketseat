74019370e7c39dc0ad2a4212a5fbe886a0b56c2b
Kelvin Musselli <38707732+jackzinn@users.noreply.github.com>
2020-01-07 21:08:17 -0300

    criado metodos, get, post, put, delete e getbyid

--- NodeJS/modulo01/index.js
+++ NodeJS/modulo01/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 
 const server = express();
-server.use(express.json()); //se não colocar este parametro vc nao consegue mandar body json
 
+//se não colocar este parametro vc nao consegue mandar body json
+server.use(express.json());
+
+// get by query
 server.get("/query", (req, res) => {
   const nome = req.query.nome;
   return res.json({ message: `Hello ${nome}` });
 });
 
+//get params
 server.get("/routes/:id", (req, res) => {
   const id = req.params.id;
   return res.json({ message: `Hello User Você Me Encontrou => Id: ${id}` });
@@ -15,21 +19,44 @@ server.get("/routes/:id", (req, res) => {
 
 // VARIAVEL DOS USERS
 const users = ["Kelvin", "Marcos", "Joana"];
+
 // GET
 server.get("/users", (req, res) => {
   return res.json(users);
 });
+
 //GET BY INDEX 'ID'
 server.get(`/user/:index`, (req, res) => {
   const { index } = req.params;
+
   return res.json(users[index]);
 });
+
 // POST
 server.post("/user", (req, res) => {
   // desestruturado;
   const { name } = req.body;
 
   users.push(name);
+
+  return res.json(users);
+});
+
+// PUT
+server.put(`/user/:index`, (req, res) => {
+  const { name } = req.body;
+  const { index } = req.params;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+//DELETE
+server.delete(`/user/:index`, (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
 
   return res.json(users);
 });
