c1bf3fc491555ec61a0c1efa5a46ba92d95f66c3
Kelvin Musselli <kelvinmusselli22@gmail.com>
2020-02-04 00:15:31 -0300

    ok

--- NodeJS/gobarber/src/routes.js
+++ NodeJS/gobarber/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Kelvin',
-    email: 'kelvinmusselli22@gmail.com',
-    password_hash: '123456',
-  });
-
-  return res.json({ message: 'Cadastrado', data: user });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
