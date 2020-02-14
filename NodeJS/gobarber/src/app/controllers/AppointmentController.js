965ccafe4e1ec7cbe70b1b4c272b9cc8e8c0341c
Kelvin Musselli <kelvinmusselli22@gmail.com>
2020-02-14 00:34:47 -0300

    criado as notificações... falta limpar elas agora

--- NodeJS/gobarber/src/app/controllers/AppointmentController.js
+++ NodeJS/gobarber/src/app/controllers/AppointmentController.js
@@ -1,12 +1,14 @@
-import Appointment from '../models/Appointment';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import * as Yup from 'yup';
+import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
-    //parametro de paginação caso não traga nada sempre vira a primeira pagina
+    // parametro de paginação caso não traga nada sempre vira a primeira pagina
     const { page = 1 } = req.query;
 
     const appointments = await Appointment.findAll({
@@ -40,7 +42,8 @@ class AppointmentController {
     });
   }
 
-  //MARCANDO UM HORARIO
+  // /////////////////////
+  // MARCANDO UM HORARIO//
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -53,7 +56,7 @@ class AppointmentController {
 
     const { provider_id, date } = req.body;
 
-    //Verificando se o provider é mesmo um provedor de serviços
+    // Verificando se o provider é mesmo um provedor de serviços
     const checkIsProvider = await User.findOne({
       where: {
         id: provider_id,
@@ -61,24 +64,24 @@ class AppointmentController {
       },
     });
 
-    //CASO NAO SEJA PROVEDOR DE SERVIÇOS
+    // CASO NAO SEJA PROVEDOR DE SERVIÇOS
     if (!checkIsProvider) {
       return res
         .status(401)
         .json({ error: 'Este usuário não é um provedor de serviços' });
     }
 
-    //ESTA VAR ARRENDONDA O HORARIO CASO ESTEJA ERRADO -> 18:30 FICARÁ 19H
+    // ESTA VAR ARRENDONDA O HORARIO CASO ESTEJA ERRADO -> 18:30 FICARÁ 19H
     const hourStart = startOfHour(parseISO(date));
 
-    //este if verifica se o usuário não quer agenda um horario ou data retroativa
+    // este if verifica se o usuário não quer agenda um horario ou data retroativa
     if (isBefore(hourStart, new Date())) {
       return res
         .status(400)
         .json({ error: 'Desculpe não é possível agendar este horário ' });
     }
 
-    //VERIFICANDO SE NÃO HÁ DISPONIBILIDADE DE HORARIO NO BANCO DE DADOS
+    // VERIFICANDO SE NÃO HÁ DISPONIBILIDADE DE HORARIO NO BANCO DE DADOS
     const checkAvailability = await Appointment.findOne({
       where: {
         provider_id,
@@ -93,11 +96,24 @@ class AppointmentController {
       });
     }
 
-    //ocorreu tudo certo vamos salvar agora
+    // ocorreu tudo certo vamos salvar agora
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    const userNameLogged = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM ', às' H:mm 'h'",
+      { locale: pt }
+    );
+
+    // enviar uma notificao apos criar um apontamento
+    await Notification.create({
+      content: `Novo agendamento de ${userNameLogged.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json({
