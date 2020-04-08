5a2524dadde4ccdb1b996319db1e2aed143233a0
Kelvin <kelvinmusselli22@gmail.com>
2020-04-07 23:43:29 -0300

    ok

--- React/flux/src/styles/global.js
+++ React/flux/src/styles/global.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 import background from '../assets/images/background.svg';
 
-export default createGlobalStyle`=
+export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Roboto:400,700&display=swap');
 
   *{
